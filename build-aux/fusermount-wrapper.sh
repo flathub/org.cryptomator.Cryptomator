d7214e17254e0c54a57322c3f663d34c0cf39f81
Armin Schrenk <armin.schrenk@skymatic.de>
2025-08-21 14:42:42 +0200

    reduce fusermount wrapper to just watch bus

--- build-aux/fusermount-wrapper.sh
+++ build-aux/fusermount-wrapper.sh
@@ -8,8 +8,4 @@ else
     FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
 fi
 
-if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
-    FD_ARGS="$FD_ARGS --forward-fd=3"
-fi
-
-exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@"
+exec flatpak-spawn --host --watch-bus $FD_ARGS fusermount3 "$@"
